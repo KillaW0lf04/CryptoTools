c6a89f79bd619d92857d4e27d14a4e241dfbc2c3
Michael Aquilina <michaelaquilina@gmail.com>
2013-08-14 21:17:42 +0200

    Removed useless test (Due to using CFB)

--- tests/aes_test.py
+++ tests/aes_test.py
@@ -5,21 +5,6 @@ from Crypto import Random
 from aes import encode_message, decode_message
 
 
-# Test to check if pycrypto is working well
-def test_pycrypto_aes():
-    prng = Random.new()
-    key = prng.read(16)
-    iv = prng.read(AES.block_size)
-
-    assert len(iv) == 16
-    assert len(key) == 16
-
-    cipher = AES.new(key, AES.MODE_CFB, iv)
-
-    # Assert result is a multiple of 16
-    assert len(cipher.encrypt('Test Text')) % 16 == 0
-
-
 def test_integrity():
     key = PBKDF1('Password1', '12345678', 16)
     plaintext = 'Test Text'
