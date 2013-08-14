a6472f906d6bad0d4fe96df6241552c5c95a46ff
Michael Aquilina <michaelaquilina@gmail.com>
2013-08-14 19:43:19 +0200

    Added test for pycrypto functionality.

--- tests/aes_test.py
+++ tests/aes_test.py
@@ -1,5 +1,23 @@
 from Crypto.Protocol.KDF import PBKDF1
+from Crypto.Cipher import AES
+from Crypto import Random
+
 from aes import encode_message, decode_message
+
+
+# Test to check if pycrypto is working well
+def test_pycrypto_aes():
+    prng = Random.new()
+    key = prng.read(16)
+    iv = prng.read(AES.block_size)
+
+    assert len(iv) == 16
+    assert len(key) == 16
+
+    cipher = AES.new(key, AES.MODE_CFB, iv)
+
+    # Assert result is a multiple of 16
+    assert len(cipher.encrypt('Test Text')) % 16 == 0
 
 
 def test_integrity():
