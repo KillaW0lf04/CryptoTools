eca3dfe548e7eaecfa20a6c4a37710edc0a073b3
Michael Aquilina <michaelaquilina@gmail.com>
2013-08-15 17:45:49 +0200

    Added privacy test for aes.

--- src/tests/aes_test.py
+++ src/tests/aes_test.py
@@ -1,11 +1,22 @@
-from Crypto.Protocol.KDF import PBKDF1
+from Crypto import Random
 
 from src.aes import encrypt_message, decrypt_message
 
 
 def test_integrity():
-    key = PBKDF1('Password1', '12345678', 16)
     plaintext = 'Test Text'
+    key = Random.new().read(16)
 
     # Ensure that D(k, E(k, p)) == p
     assert decrypt_message(key, encrypt_message(key, plaintext)) == plaintext
+
+
+def test_privacy():
+    plaintext = 'Test Text'
+    prng = Random.new()
+
+    key1 = prng.read(16)
+    key2 = prng.read(16)
+
+    # Ensure decrypting with a different key does not reveal the plaintext
+    assert decrypt_message(key1, encrypt_message(key2, plaintext)) != plaintext
