3eaf11e8b9ceb3ffbe3d2d9b2fc34d0dc209d405
Michael Aquilina <michaelaquilina@gmail.com>
2013-08-16 13:50:28 +0200

    Add zip2 function to utils. Add test_zip2 to utils_test.

--- src/tests/utils_test.py
+++ src/tests/utils_test.py
@@ -1,7 +1,21 @@
-from src.utils import is_package_installed
+from src.utils import is_package_installed, zip2
 
 
 def test_is_package_installed():
     assert not is_package_installed('ThisPackageDoesNotExist')
     assert is_package_installed('PyCrypto', '2.6')
     assert is_package_installed('PyCrypto')
+
+
+def test_zip2():
+    text1 = 'hello'
+    text2 = 'hello world'
+
+    # Resultant length should be the length of the largest text
+    assert len(zip2(text1, text2)) == len(text2)
+    assert len(zip2(text2, text1)) == len(text2)
+
+    result = zip2(text1, text2)
+
+    assert ''.join([c1 for c1, _ in result]) == 'hellohelloh'
+    assert ''.join([c2 for _, c2 in result]) == 'hello world'
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 __author__ = 'Michael Aquilina'
 
+import math
 import pkg_resources
 from distutils.version import StrictVersion
 
@@ -16,3 +17,14 @@ def is_package_installed(package_name, min_required_version=None):
         installed = StrictVersion(package_version) >= StrictVersion(min_required_version)
 
     return installed
+
+
+def zip2(text1, text2):
+    large_text = text1 if len(text1) > len(text2) else text2
+    small_text = text2 if len(text2) < len(text1) else text1
+
+    # Expand the small text
+    mul_value = int((math.ceil(len(large_text) / float(len(small_text)))))
+    small_text = small_text * mul_value
+
+    return zip(small_text, large_text)
